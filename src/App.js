369f7382aa69a482548ece66a499ff5daa949857
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-05 20:11:12 +0200

    Add Header, Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Header from 'components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import Router from 'routes';
+
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <BrowserRouter basename="/coffee-shop">
+      <div className="app">
+        <Header />
+        <main className="content">
+          <Router />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
