b00d0bca243ae60c9a2efaa39bbcca0b0b8644c2
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-11 23:44:04 +0200

    Add cards to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import ComercialCard from 'components/ComercialCard';
 import React from 'react';
 
 import './home.scss';
@@ -5,8 +6,18 @@ import './home.scss';
 const Home = () => {
   return (
     <main>
-      Home
-      <p>home</p>
+      <div className="comercial">
+        <ComercialCard
+          className="comercial__card"
+          heading="Sign up for our newsletter"
+          description="Be the first to know when we launch a new product or when we need your support to rally for a cause."
+        />
+        <ComercialCard
+          className="comercial__card"
+          heading="Follow us"
+          description="You know the drill. Show your support by following and sharing our message on social media.."
+        />
+      </div>
     </main>
   );
 };
