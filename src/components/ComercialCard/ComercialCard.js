326c1b829a293e8b17c644fdb3e3964f68c3ca70
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-11 19:15:01 +0200

    Add props for ComercialCard

--- src/components/ComercialCard/ComercialCard.js
+++ src/components/ComercialCard/ComercialCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// import classNames from 'classnames';
 
-const ComercialCard = () => {
+const ComercialCard = ({ heading, description, color, children }) => {
   return (
     <article>
-      <h1>Title</h1>
-      <p>More info</p>
+      <h1>{heading}</h1>
+      <p>{description}</p>
+      <div>{children}</div>
     </article>
   );
+};
+
+ComercialCard.propTypes = {
+  heading: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
 
 export default ComercialCard;
