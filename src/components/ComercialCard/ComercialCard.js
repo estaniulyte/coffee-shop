b9ff9bc72400ed74c4e6ac862bccda61ccff6c5d
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-14 19:35:27 +0200

    Add theme prop to comercial card

--- src/components/ComercialCard/ComercialCard.js
+++ src/components/ComercialCard/ComercialCard.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 
 import './comercial-card.scss';
 
-const ComercialCard = ({ heading, description, color, children }) => {
+const ComercialCard = ({ heading, description, theme = 'light', children }) => {
   return (
-    <article className="carde">
-      <h1 className="carde__heading">{heading}</h1>
-      <p className="carde__description">{description}</p>
+    <article
+      className={classNames('card', {
+        [`card--${theme}`]: true,
+      })}>
+      <h1 className="card__heading">{heading}</h1>
+      <p className="card__description">{description}</p>
       <div>{children}</div>
     </article>
   );
 };
 
+const themes = ['light', 'dark'];
+
 ComercialCard.propTypes = {
   heading: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  color: PropTypes.string,
+  theme: PropTypes.oneOf(themes),
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
 
