53acde57431c545300714a15882b06acbb8f9158
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-15 23:30:48 +0200

    Remove Comercial section and add SocialSection component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import ComercialCard from 'components/ComercialCard';
+import SocialSection from 'components/Header/components/SocialSection';
 import React from 'react';
 
 import './home.scss';
@@ -6,18 +6,7 @@ import './home.scss';
 const Home = () => {
   return (
     <main>
-      <div className="comercial">
-        <ComercialCard
-          className="comercial__card"
-          heading="Sign up for our newsletter"
-          description="Be the first to know when we launch a new product or when we need your support to rally for a cause."
-        />
-        <ComercialCard
-          className="comercial__card"
-          heading="Follow us"
-          description="You know the drill. Show your support by following and sharing our message on social media.."
-        />
-      </div>
+      <SocialSection />
     </main>
   );
 };
